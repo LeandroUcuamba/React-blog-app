147652d76f1f5f924843bd3ce50e16efaffa6932
Leandro Ucuamba <leandrodossantos.lc5@gmail.com>
2024-05-19 18:35:04 +0100

    fix_id_type-and_useReactHookForm

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,24 +1,32 @@
+import { api } from '../../lib/axios'
+import { useForm } from "react-hook-form";
 import "./styles.css";
 
 export function Form({ title, textButton }) {
 
+  const { register, handleSubmit, reset } = useForm();
+
+  function handleCreatePost(data){
+     api.post('/posts', data)
+     reset();
+  }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit(handleCreatePost)}>
       <h2>{ title }</h2>
       <div className="field">
-        <input placeholder="Título" />
+        <input placeholder="Título" {...register("title")}/>
       </div>
 
       <div className="field">
-        <input placeholder="Descrição" />
+        <input placeholder="Descrição" {...register("description")}/>
       </div>
 
       <div className="field">
-        <textarea placeholder="Descrição" />
+        <textarea placeholder="Conteúdo" {...register("content")}/>
       </div>
 
-      <button>{ textButton }</button>
+      <button type="submit">{ textButton }</button>
     </form>
   );
 }
